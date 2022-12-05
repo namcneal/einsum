db05745849de5ef3e41220cde653beb33f4c0e41
pass <pass@gmail.com>
2022-12-05 13:25:11 +0100

    My goal is to change enough of the library so that I do not get the following erro <<expected trait object dyn ArrayLike, found struct ArrayBase>> when using the einsum function

--- src/optimizers.rs
+++ src/optimizers.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
 
 //! Methods to produce a `ContractionOrder`, specifying what order in which to perform pairwise contractions between tensors
 //! in order to perform the full contraction.
